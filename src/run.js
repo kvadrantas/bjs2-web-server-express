f892eea80c5111bb7159d1af08a0194fcf81fb6b
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-09-09 11:43:24 +0300

    Created some order

--- src/run.js
+++ src/run.js
@@ -1,8 +1,38 @@
+// Fast, unopinionated, minimalist web framework for node.
+// const express = require('express')
+// const app = express()
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// app.listen(3000)
+
+// HTML URL STRUCTURE
+// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL
+
+// EXPRESS SERVER HELP 
+// http://expressjs.com/en/5x/api.html#req.query
+
+// HANDLERS HELP
+// https://handlebarsjs.com/guide/#what-is-handlebars
+// *****************************************************************************
+
+
+// MAIN/DEFAULT WEB SERVER PARAMETERS
 import express from "express";
+const app = express();
+const PORT = 3000;    // Sets default website port
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
-const PORT = 3000;
 const WEB = "web";
+app.use(express.static(WEB, {    // Like "Default Document" on ISS
+  index: ["index.html"]
+}));
 
+
+
+// DATA
 let nextId = 1;
 const zmones = [
   {
@@ -25,24 +55,14 @@ const zmones = [
   },
 ];
 
-const app = express();
 
-app.use(express.static(WEB, {
-  index: ["index.html"]
-}));
 
-app.get("/labas", (req, res) => {
-  console.log(req.ip);
-  console.log(req.method);
-  console.log(req.path);
-  console.log(req.query);
-  res.send("labas");
-});
-
+// DATA RENDERING
 app.get("/zmones", (req, res) => {
   let html = "";
   html += "<html>\r\n";
   html += "<body>\r\n";
+  html += "<img style = 'height:60px;' src=./mdn-url-all.png>";
   html += "<h1>Žmonių sąrašas</h1>\r\n";
   html += "<ul>\r\n";
   for (const zmogus of zmones) {
@@ -59,6 +79,7 @@ app.get("/zmones", (req, res) => {
   res.send(html);
 });
 
+// DELETING USER
 app.get("/zmogusDelete", (req, res) => {
   const id = parseInt(req.query.id);
   const index = zmones.findIndex(e => e.id === id);
@@ -68,6 +89,17 @@ app.get("/zmogusDelete", (req, res) => {
   res.redirect("/zmones");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+
+
+
+
+
+// *****************************************************************************
+// TEST INFO
+// app.get("/labas", (req, res) => {
+//   console.log(req.ip);
+//   console.log(req.method);
+//   console.log(req.path);
+//   console.log(req.query);
+//   res.send("labas");
+// });
